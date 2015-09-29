9628a62cced41c3a80b9822c00a7710fd251fee2
unknown <nmartin@NMARTIN-T35.ciqdev.com>
2015-09-29 16:37:24 -0400

    Initial commit of Captrictity file upload API project

---
+++ Captricity/Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace Captricity
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        static string _fileUploadDestination = "ImageUploads";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            if(Request.Files.Count > 0)
+            {
+                HttpFileCollection uploadedFiles = Request.Files;
+
+                
+                try
+                {
+                    int count = 0;
+                    List<string> rejectedFiles = new List<string>();
+
+                    for(int i = 0; i < uploadedFiles.Count; i++)
+                    {
+                        HttpPostedFile userPostedFile = uploadedFiles[i];
+
+                        if(userPostedFile.ContentType == "image/jpeg"
+                            || userPostedFile.ContentType == "image/png"
+                            || userPostedFile.ContentType == "application/pdf")
+                        {
+                            if(userPostedFile.ContentLength < (1024 * 4000))
+                            {
+                                string filename = Path.GetFileName(userPostedFile.FileName);
+                                userPostedFile.SaveAs(Server.MapPath("~/" + _fileUploadDestination + "/") + filename);
+                                StatusLabel.Text = "Upload status: " + filename + " uploaded!";
+                                count++;
+                            }
+                            else
+                            {
+                                StatusLabel.Text = "Upload status: The file has to be less than 4000 kb!";
+                                rejectedFiles.Add(userPostedFile.FileName);
+                            }
+                        }
+                        else
+                        {
+                            StatusLabel.Text = "Upload status: Only JPEG, PNG, or PDF files are accepted!";
+                            rejectedFiles.Add(userPostedFile.FileName);
+                        }
+                    }
+
+                    StatusLabel.Text = "Upload status: " + count + " files uploaded!";
+                    StatusLabel.Text += "Files rejected: ";
+
+                    foreach(string reject in rejectedFiles)
+                    {
+                        StatusLabel.Text += " " + reject;
+                    }
+
+                }
+                catch(Exception ex)
+                {
+                    StatusLabel.Text = "Upload status: The files could not be uploaded. The following error occured: " + ex.Message;
+                }
+
+                
+            }
+        }
+    }
+}
---
+++ Captricity/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Captricity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Captricity")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5946d734-c4ff-4e36-9292-da7f30835afc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
